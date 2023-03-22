b017f47751240a6fe09563873364f0f220c18a9c
fhqhxm0727 <fhqhxm0727@gmail.com>
2023-03-22 11:31:17 +0900

    Q4를 반복하여 실행하고, 0 to 2의 값이 아닌 경우 프로그램 실행횟수 출력 및 종료

---
+++ 20230322/Q5.py
@@ -0,0 +1,22 @@
+import random
+
+gnum = 0
+
+while True :
+    com= random.randrange(3)
+    user= int(input('가위0 바위1 보2 선택: '))
+    if (user == 0 or user == 1 or user == 2) :
+        print('결과 user= %d, com= %d' % (user, com))
+
+        if user == com :
+            print('비겼습니다!')
+        elif (user == 0 & com == 2) | (user == 1 & com == 0) | (user == 2 & com == 1) :
+            print ("user가 이겼습니다!")
+        else :
+            print("com이 이겼습니다...")
+        gnum = gnum + 1
+
+        
+    else : break
+
+print('가위바위보를 %d회 플레이 하셨습니다!' % gnum)
