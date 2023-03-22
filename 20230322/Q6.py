b13cf54de034a12d18b9fea061291110e5092956
fhqhxm0727 <fhqhxm0727@gmail.com>
2023-03-22 12:14:27 +0900

    반복문기초: while문과 for문을 이용하여 짝수의 총합 구하기

--- 20230322/Q6.py
+++ 20230322/Q6.py
@@ -1,17 +1,17 @@
 n = int(input('어디까지 짝수를 합할까요? : '))
-i = 1
+i = 2
 sum = 0
 
 
 while i <= n :
-    if i % 2 == 0 : sum = sum + i 
-    i = i + 1
+    sum = sum + i 
+    i = i + 2
 
 print('1부터 %d까지의 짝수 합은 %d\n' % (n, sum))
 
-i = 0
+i = 2
 sum = 0
-for i in range(1, n+1) :
-    if i % 2 == 0 : sum = sum + i
+for i in range(2, n+1, 2) :
+    sum = sum + i
      
 print('1부터 %d까지의 짝수 합은 %d\n' % (n, sum))
