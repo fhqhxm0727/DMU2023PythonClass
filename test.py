d72dd032b61706f095bd2e7c37cf2a94564ea0d9
fhqhxm0727 <fhqhxm0727@gmail.com>
2023-03-25 16:49:22 +0900

    Time 모듈 연습 : 간단한 남은시간 카운트 코드

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-print("hello Git!")
-print("hello hello")
+
+
+from time import time
+from time import gmtime
+from time import localtime
+from time import sleep
+
+
+print("입력한 시간까지 몇시간 몇분 몇초 남았는지 알려주고, 해당 시간에 종료되는 프로그램입니다!")
+wantHour = int(input("원하는 시 입력 : "))
+wantMinute = int(input("원하는 분 입력 : "))
+
+
+tm = localtime(time())
+print("%d시 %d분" % (tm.tm_hour, tm.tm_min))
+sleep(1)
